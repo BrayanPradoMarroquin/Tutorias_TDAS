56a073190929ab183fd667d26600fff7b4b83ed2
Brayan-PM <78266710+BrayanPradoMarroquin@users.noreply.github.com>
2022-02-08 23:35:37 -0600

    Ejemplo basico de Lista enlazada Simple

---
+++ LISTAS_SIMPLES/SIMPLE/ListaSimple.py
@@ -0,0 +1,49 @@
+from NodoSimple import Nodo
+import os
+
+class Lista:
+    def __init__(self):
+        self.cabeza = None
+
+    def vacio(self):
+        return self.cabeza == None
+
+    def agregar(self, id, nombre):
+        Nuevo = Nodo(id, nombre)
+        if self.vacio():
+            self.cabeza = Nuevo
+        else:
+            Nuevo.siguiente = self.cabeza
+            self.cabeza = Nuevo 
+
+    def recorrer(self):
+        Nuevo = self.cabeza
+        while(Nuevo != None):
+            print(str(Nuevo.getid()) + " -> " + Nuevo.getnombre())
+            Nuevo = Nuevo.siguiente
+
+    def graficar(self):
+        Nuevo = self.cabeza
+        cont = 0
+        cadena = ""
+        file = open('grafica.dot', 'w')
+        cadena = cadena + 'graph G { \n'
+        while(Nuevo!=None):
+            cadena = cadena + 'Node'+str(cont)+'[label=\"'+str(Nuevo.getid())+" -> "+Nuevo.getnombre()+'\"];\n'
+            if(Nuevo!=self.cabeza):
+                cadena = cadena + 'Node'+str(cont-1)+' -- '+'Node'+str(cont)+';\n'
+            Nuevo = Nuevo.siguiente
+            cont+=1
+        cadena = cadena + '}'
+        file.write(cadena)
+        file.close()
+        os.system('dot -Tpng grafica.dot -o grafica.png')
+        os.startfile("grafica.png")
+
+
+Nuevo = Lista()
+Nuevo.agregar(1, "hola")
+Nuevo.agregar(1, "otro")
+Nuevo.agregar(3, "cinco")
+Nuevo.recorrer()
+Nuevo.graficar()
---
+++ LISTAS_SIMPLES/SIMPLE/NodoSimple.py
@@ -0,0 +1,19 @@
+class Nodo:
+    def __init__(self, id, nombre):
+        self.__id = id
+        self.__nombre = nombre
+        self.siguiente = None
+
+    def getid(self):
+        return self.__id
+
+    def getnombre(self):
+        return self.__nombre
+
+
+    def setid(self, id):
+        self.__id = id
+
+    def setnombre(self, nombre):
+        self.__nombre = nombre
+    
